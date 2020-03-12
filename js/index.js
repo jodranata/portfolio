02105a68e1e76e76e09eb4e5ddee7e997eb34c18
Jodi Pranata <jodranata@gmail.com>
2020-03-12 23:04:42 +0700

    add project section image

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 $(function() {
+  const navButton = document.querySelector('.navbar-lists');
   $('.hamburger-button').on('click', function(e) {
     $('.navbar-lists').toggleClass('display');
     $('.nav-container').toggleClass('expand');
     e.preventDefault();
   });
+  function collapseNav() {
+    $('.navbar-lists').removeClass('display');
+    $('.nav-container').removeClass('expand');
+  }
 });
